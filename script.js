24747974ad0bd0e8c2868f1fcd01aa1967675ea3
Sagar Gupta <sagar2003gupta@gmail.com>
2022-12-28 06:15:05 +0530

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let a = document.querySelectorAll(".portfolio-image-container");
+a[0].addEventListener('click', function(){
+        document.location = "https://decoder2201.github.io/MultiTasking_App/";
+    });
+a[1].addEventListener('click', function(){
+        document.location = "https://decoder2201.github.io/City-Restaurant-Project";
+    });
+a[2].addEventListener('click', function(){
+        document.location = "https://decoder2002.github.io/Mini-Project/";
+    });
